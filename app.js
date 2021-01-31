9ae8174a731778ac380c7ef5c7a6ad0a9700daec
Julie Lam <lam.julie@hotmail.com>
2021-02-01 08:48:07 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.burger-menu');
+const navLinks = document.querySelector('.nav-links'); 
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active')
+})
+
+navLinks.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active')
+})
